31e563418e82228a83e8f3376e428ecef25ab503
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-05-04 23:42:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+const chefData = require('./data/chefs.json')
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Chefs recipe server running')
+});
+
+
+app.get('/chefs', (req, res) => {
+    res.send(chefData);
+})
+
+app.get("/chefs/:id" , (req,res)=>{
+    const id = req.params.id;
+    const selectedChef =  chefData.chefs.find(cf => cf.chef_id == id);
+    res.send(selectedChef);
+})
+
+app.listen(port, () => {
+    console.log(`Recipe API is running on port: ${port}`)
+})
